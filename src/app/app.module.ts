2314aa2e6377ba2c36a1183f9674020ad840343d
Sagar Joshi <sagar.2.joshi@atos.net>
2018-10-15 00:37:50 +0530

    Added roting mofule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,   
+        
+        AppRoutingModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
